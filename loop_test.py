9de641aa60dbbe9914542690c8d936b7a144f9e3
Chris Guarino <chris.francis.guarino@gmail.com>
2025-08-14 21:59:41 -0400

    Working to use jsonl for the prompts and the tests

--- loop_test.py
+++ loop_test.py
@@ -6,47 +6,6 @@ from support import *  # your judge
 LOG_PATH = "logs/runs.jsonl"
 MODEL_ID = "ppo-llama"
 
-##### Test Prompts #####
-
-# Adding
-# PROMPT = """The following function is supposed to add two numbers, but it has a bug.
-# Return ONLY the corrected Python function. Do NOT include backticks or explanation.
-# Start your answer with: def add(
-
-# def add(a, b):
-#     return a - b
-# """
-
-# Palindrome
-PROMPT = """You are fixing a buggy Python function.
-Return ONLY the corrected Python function. Do NOT include backticks or explanations.
-Start your answer with: def is_palindrome(
-
-Requirements:
-- Return True if s is a palindrome ignoring CASE and SPACES.
-
-# Buggy version:
-def is_palindrome(s):
-    return s == s[::-1]
-"""
-########################
-
-##### Test Data #####
-
-tests = [
-    ("racecar", True),
-    ("Race Car", True),
-    ("nurses run", True),
-    ("hello", False),
-    ("A man a plan a canal Panama", True),
-    ("", True),
-    ("   ", True),
-    ("abc cba", True),
-    ("ab!ba", True),  
-    ("snakes", False)
-]
-#####################
-
 def one_run(temp=0.9):
     #1) Call local LLM
     resp = ollama.generate(model=MODEL_ID, prompt=PROMPT, options={"temperature": temp})
